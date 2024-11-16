8eb161b33616ef7da752fd72628ad2b33bf76626
younes ferhat <y.ferhat@esi-sba.dz>
2024-11-16 21:19:20 +0100

    main

--- main.cpp
+++ main.cpp
@@ -7,19 +7,6 @@ using namespace std;
 
 int main() 
 {   
-   std::vector<std::vector<char>> labyrinthe = {
-        {'X', 'X', 'X', 'X', 'X', 'X', 'X'},
-        {'X', '.', '.', '.', 'X', '.', 'X'},
-        {'X', '.', 'X', '.', 'X', '.', 'X'},
-        {'X', '.', 'X', '.', '.', '.', 'X'},
-        {'X', '.', 'X', 'X', 'X', 'X', 'X'},
-        {'X', '.', '.', '.', '.', '.', 'X'},
-        {'X', 'X', 'X', 'X', 'X', 'X', 'X'}
-    };
-
-    // Créer l'instance de mainDroite pour résoudre le labyrinthe
-    mainDroite robotSolver(labyrinthe, 1, 1, 'R'); // Position (1,1), orienté à droite ('R')
-    robotSolver.resoudre();
-
+   std::cout<<"hello world !";
     return 0;
 }
