634af930439babf192dec81130aebe19a1f1b8f5
amayas <amayas.chouchane@uha.fr>
2024-11-17 16:20:55 +0100

    régler le problème de main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "C:\Users\youne\Desktop\prjct labyrinth\gestion-des-d-placements-d-un-robot-dans-un-labyrinthe\headers\mainDroite.h"
-using namespace std;
+
 
 
 int main() 
